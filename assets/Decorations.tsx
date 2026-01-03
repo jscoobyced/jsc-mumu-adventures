3f6e53465c58c5e3b1ecb4463cf6446c1778704e
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2026-01-03 11:49:13 +0700

    Update images path

--- assets/Decorations.tsx
+++ assets/Decorations.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Decorations" tilewidth="16" tileheight="16" tilecount="2448" columns="36">
- <image source="../src/images/decorations.png" width="576" height="1088"/>
+ <image source="../src/public/decorations.png" width="576" height="1088"/>
 </tileset>
