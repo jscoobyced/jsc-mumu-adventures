6f0d3265655a53b31b5b05bebb60399afc49f49d
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2026-01-19 20:20:18 +0700

    Re-enable splash screen

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,11 +1,9 @@
-// import { intro } from './intro'
+import { intro } from './intro'
 import { initializeEventListeners } from './utils/eventListeners'
-import { startGame } from './utils/game'
 
 initializeEventListeners()
 const myFont = new FontFace('MumuFont', 'url(fonts/mumu.ttf)')
 myFont.load().then(() => {
   document.fonts.add(myFont)
-  startGame()
-  // intro()
+  intro()
 })
