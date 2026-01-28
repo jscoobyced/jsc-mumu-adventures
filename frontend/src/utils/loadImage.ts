25ed6ececed9988070e93c944937bfaab569e74d
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2026-01-28 21:40:09 +0700

    Fix redundant load of images

--- frontend/src/utils/loadImage.ts
+++ frontend/src/utils/loadImage.ts
@@ -1,7 +1,22 @@
+// Keep a list of loaded images to prevent redundant loading
+const loadedImages: Record<string, HTMLImageElement> = {}
+
+/**
+ * Loads an image from the given source URL.
+ * Caches the loaded image to avoid redundant network requests.
+ * @param src - The source URL of the image to load.
+ * @returns A promise that resolves to the loaded HTMLImageElement.
+ */
 export const loadImage = (src: string): Promise<HTMLImageElement> => {
+  if (loadedImages[src]) {
+    return Promise.resolve(loadedImages[src])
+  }
   return new Promise((resolve, reject) => {
     const img = new Image()
-    img.onload = () => resolve(img)
+    img.onload = () => {
+      loadedImages[src] = img
+      resolve(img)
+    }
     img.onerror = reject
     img.src = src
   })
